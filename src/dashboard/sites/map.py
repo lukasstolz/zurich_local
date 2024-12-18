7692b22d4f8bae21d42328a97d66f77a41ed0905
lukas <stolz.lukas@gmail.com>
2024-12-18 23:32:42 +0100

    refactor map page, no map reloads

--- src/dashboard/sites/map.py
+++ src/dashboard/sites/map.py
@@ -5,44 +5,77 @@ from streamlit_folium import st_folium
 
 from src.dashboard.data.load_data import load_categories, load_zurich_items
 
-st.subheader("Baz")
-
 items_df = load_zurich_items()
 
 
-if "selected_type" not in st.session_state:
-    st.session_state["category"] = "Gastronomie"
+def create_marker(lat: int, lon: int, popup: str, tooltip: str) -> None:
+    marker = folium.Marker([lat, lon], popup=popup, tooltip=tooltip)
+    st.session_state["markers"].append(marker)
 
 
-# Sidebar
+@st.cache_data
+def base_map() -> None:
+    m = folium.Map(
+        location=st.session_state.map_config["center"],
+        zoom_start=st.session_state.map_config["zoom"],
+        tiles="Stadia.AlidadeSmooth",
+    )
+    fg = folium.FeatureGroup(name="Markers")
+
+    return m, fg
+
+
+def draw_map() -> None:
+    m, fg = base_map()
+
+    for marker in st.session_state["markers"]:
+        fg.add_child(marker)
+
+    st_folium(
+        m,
+        feature_group_to_add=fg,
+        center=st.session_state.map_config["center"],
+        zoom=st.session_state.map_config["zoom"],
+        returned_objects=[],
+        use_container_width=True,
+    )
+
+
+def add_markers() -> None:
+    selected_items = items_df[items_df["category_de"] == st.session_state["category"]]
+
+    selected_items[~pd.isna(selected_items["geocoordinates_latitude"])].apply(
+        lambda x: create_marker(
+            x["geocoordinates_latitude"],
+            x["geocoordinates_longitude"],
+            tooltip=x["name_de"],
+            popup=x["description_de"],
+        ),
+        axis=1,
+    )
+
+
 with st.sidebar:
     categories_df = load_categories()
 
     for category in categories_df["category_de"]:
         if st.button(category):
+            st.session_state["markers"].clear()
             st.session_state["category"] = category
+            add_markers()
 
+if "category" not in st.session_state:
+    st.session_state["category"] = "Gastronomie"
 
-m = folium.Map(location=[47.3769, 8.5417], zoom_start=12, tiles="Stadia.AlidadeSmooth")
+if "markers" not in st.session_state:
+    st.session_state["markers"] = []
+    add_markers()
 
+if "map_config" not in st.session_state:
+    st.session_state.map_config = {
+        "center": [47.3769, 8.5417],
+        "zoom": 12,
+    }
 
-def add_item_marker(lat: int, lon: int, popup: str, tooltip: str) -> None:
-    folium.Marker([lat, lon], popup=popup, tooltip=tooltip).add_to(m)
-
-
-selected_items = items_df[items_df["category_de"] == st.session_state["category"]]
-
-selected_items[~pd.isna(selected_items["geocoordinates_latitude"])].apply(
-    lambda x: add_item_marker(
-        x["geocoordinates_latitude"],
-        x["geocoordinates_longitude"],
-        tooltip=x["name_de"],
-        popup=x["description_de"],
-    ),
-    axis=1,
-)
-
-st_folium(m, width=1200, height=800, returned_objects=[])
-
-
-st.dataframe(selected_items)
+st.header(st.session_state["category"], divider="rainbow")
+draw_map()
