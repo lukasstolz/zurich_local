63cc0feee95fd37308db80564f033cb90f0b1810
lukas <stolz.lukas@gmail.com>
2024-12-18 18:07:42 +0100

    add map to dashboard

--- src/dashboard/data/load_data.py
+++ src/dashboard/data/load_data.py
@@ -1,4 +1,56 @@
-import duckdb
+import os
 
-# connect to MotherDuck using 'md:' or 'motherduck:'
-con = duckdb.connect("md:")
+import duckdb
+import pandas as pd
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+md_access_token = os.environ.get("MD_ACCESS_TOKEN")
+
+con: duckdb.DuckDBPyConnection = duckdb.connect(
+    f"md:?motherduck_token={md_access_token}"
+)
+con.sql("use zuerich_db")
+
+
+@st.cache_data
+def load_cities() -> pd.DataFrame:
+    cities = con.sql("select * from marts.dim_cities").df()
+    return cities
+
+
+@st.cache_data
+def load_types() -> pd.DataFrame:
+    types = con.sql("select * from marts.dim_types").df()
+    return types
+
+
+@st.cache_data
+def load_custom_types() -> pd.DataFrame:
+    custom_types = con.sql("select * from marts.dim_custom_types").df()
+    return custom_types
+
+
+@st.cache_data
+def load_zurich_items() -> pd.DataFrame:
+    items = con.sql("""
+                    select
+                        *
+                    from marts.fact_zurich_items items
+
+                    left join marts.dim_types types
+                    on types.id = items.type_id
+
+                    left join marts.dim_custom_types custom_types
+                    on custom_types.id = items.custom_type_id
+
+                    left join marts.dim_cities cities
+                    on cities.id = items.city_id
+
+                    """).df()
+
+    return items
+
+
+print(load_zurich_items())
--- src/dashboard/sites/map.py
+++ src/dashboard/sites/map.py
@@ -1,7 +1,54 @@
+import re
+
+import folium
+import pandas as pd
 import streamlit as st
+from streamlit_folium import st_folium
+
+from src.dashboard.data.load_data import load_types, load_zurich_items
 
 st.subheader("Baz")
 
+items_df = load_zurich_items()
+
+
+if "selected_type" not in st.session_state:
+    st.session_state["type"] = "Restaurant"
+
+
 # Sidebar
 with st.sidebar:
-    zone_options = st.multiselect("Kategorien", ["Shopping", "Museen"])
+    types_df = load_types()
+    types = [
+        re.sub(r"((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))", r" \1", type)
+        for type in types_df["type"]
+    ]
+
+    for type in types:
+        if st.button(type):
+            st.session_state["type"] = type
+
+
+m = folium.Map(location=[47.3769, 8.5417], zoom_start=12, tiles="Stadia.AlidadeSmooth")
+
+
+def add_item_marker(lat: int, lon: int, popup: str, tooltip: str) -> None:
+    folium.Marker([lat, lon], popup=popup, tooltip=tooltip).add_to(m)
+
+
+selected_items = items_df[items_df["type"] == st.session_state["type"]]
+
+selected_items[~pd.isna(selected_items["geocoordinates_latitude"])].apply(
+    lambda x: add_item_marker(
+        x["geocoordinates_latitude"],
+        x["geocoordinates_longitude"],
+        tooltip=x["name_de"],
+        popup=x["description_de"],
+    ),
+    axis=1,
+)
+
+st_folium(m, width=1200, height=800, returned_objects=[])
+
+
+st.dataframe(selected_items)
