0cd8c10709fe80b61073baf45733d8ad96eaf4e1
lukas <stolz.lukas@gmail.com>
2025-01-12 16:31:36 +0100

    Add page description

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -9,6 +9,10 @@ nav = get_nav_from_toml("src/dashboard/pages.toml")
 st.markdown(f"<style>{get_style_str()}</style>", unsafe_allow_html=True)
 st.logo("src/dashboard/static/logo/logo.png", size="large")
 
+with st.sidebar:
+    st.text(
+        "Welcome to zurich local. Learn about things to do, where to stay, eat & drink and what to see in and around Zurich!"
+    )
 
 pg = st.navigation(nav)
 
